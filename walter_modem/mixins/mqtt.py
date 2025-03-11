ef9db5f66535a2ea62eb85e4864206e94ccddeed
FoxyHunter7 <80623140+FoxyHunter7@users.noreply.github.com>
2025-03-11 11:22:40 +0100

    update mqtt mixin with proper did ring, fix config

--- walter_modem/mixins/mqtt.py
+++ walter_modem/mixins/mqtt.py
@@ -1,10 +1,11 @@
 from ..core import ModemCore
 from ..enums import (
-    ModemRspType,
-    ModemCmdType
+    ModemCmdType,
+    ModemState
 )
 from ..structs import (
-    ModemRsp
+    ModemRsp,
+    ModemMQTTResponse
 )
 from ..utils import (
     modem_string
@@ -15,7 +16,7 @@ class ModemMQTT(ModemCore):
         client_id: str,
         user_name: str,
         password: str,
-        tls_profile_id: int,
+        tls_profile_id: int = None,
         rsp: ModemRsp = None
     ) -> bool:
         """
@@ -24,9 +25,11 @@ class ModemMQTT(ModemCore):
         """
         return await self._run_cmd(
             rsp=rsp,
-            at_cmd='AT+SQNSMQTTCFG=0,{},{},{},{}'.format(
-                modem_string(client_id), modem_string(user_name),
-                modem_string(password), tls_profile_id
+            at_cmd='AT+SQNSMQTTCFG=0,{},{},{}{}'.format(
+                modem_string(client_id),
+                modem_string(user_name) if user_name else '""',
+                modem_string(password) if user_name else '""',
+                f',{tls_profile_id}' if tls_profile_id else ''
             ),
             at_rsp=b'OK'
         )
@@ -47,7 +50,7 @@ class ModemMQTT(ModemCore):
         client_id: str,
         user_name: str,
         password: str,
-        tls_profile_id: int,
+        tls_profile_id: int = None,
         rsp: ModemRsp = None
     ) -> bool:
         """
@@ -63,7 +66,7 @@ class ModemMQTT(ModemCore):
         return await self._run_cmd(
             rsp=rsp,
             at_cmd=f'AT+SQNSMQTTCONNECT=0,{modem_string(server_name)},{port}',
-            at_rsp=b'+SQNSMQTTONCONNECT:0,0'
+            at_rsp=b'+SQNSMQTTONCONNECT:0,'
         )
 
     async def mqtt_publish(self,
@@ -96,60 +99,48 @@ class ModemMQTT(ModemCore):
             at_cmd=f'AT+SQNSMQTTSUBSCRIBE=0,{modem_string(topic)},{qos}',
             at_rsp=b'+SQNSMQTTONSUBSCRIBE:0,{}'.format(modem_string(topic))
         )
-
-    async def _mqtt_receive_message(self,
-        topic: str,
-        message_id = None,
-        max_length = None,
+    
+    async def mqtt_did_ring(self,
+        msg_list: list,
+        topic: str = None,
         rsp: ModemRsp = None
-    ) -> bool:
+        ) -> bool:
         """
-        Coroutine to initiate delivery of an MQTT message by the modem
-        The payload of the MQTT message will be stored in the 'payload' property
-        of the corresponding ModemMqttMessage instance within the
-        _mqtt_messages list
+        :param msg_list: Refence to a list where the received messages will be put.
+        :param topic: The exact topic to filter on, leave as None for all topics
+        :param rsp: Reference to a modem response instance
+
+        :return bool: True on success, False on failure
         """
-        at_cmd = "AT+SQNSMQTTRCVMESSAGE=0,{}".format(modem_string(topic))
-        if message_id:
-            at_cmd += ",{}".format(message_id)
-        if max_length:
-            at_cmd += ",{}".format(max_length)
+        msg = None
+        msg_index = -1
+
+        for i in range(len(self._mqtt_msg_buffer)):
+            _msg = self._mqtt_msg_buffer[i]
+            if not _msg.free:
+                if (topic and _msg.topic == topic) or topic is None:
+                    msg = _msg
+                    msg_index = i
+                    break
+
+        if msg is None:
+            if rsp: rsp.result = ModemState.NO_DATA
+            return False
+        
+        at_cmd = f'AT+SQNSMQTTRCVMESSAGE=0,{modem_string(msg.topic)}'
+        if msg.message_id:
+            at_cmd += f',{msg.message_id}'
+
+        self._mqtt_msg_buffer[msg_index].free = True
+
+        async def complete_handler(result, rsp, complete_handler_arg):
+            rsp.mqtt_response = complete_handler_arg
 
         return await self._run_cmd(
             rsp=rsp,
+            ring_return=msg_list,
             at_cmd=at_cmd,
-            at_rsp=b'OK'
+            at_rsp=b'OK',
+            complete_handler=complete_handler,
+            complete_handler_arg=ModemMQTTResponse(msg.topic, msg.qos)
         )
-
-    async def mqtt_receive(self):
-        """
-        Coroutine to 'download' the payloads of all MQTT messages that are stored
-        in the buffer of the modem and have not yet been downloaded from the modem
-        into the controller.
-        The return value is the number of all downloaded messages.
-        """
-        n = 0 
-        for msg in self._mqtt_messages:
-            if not msg.received:
-                rsp = ModemRsp()
-                if not await self._mqtt_receive_message(msg.topic, msg.message_id, rsp=rsp):
-                    print('Failed to receive MQTT message')
-                elif rsp.type == ModemRspType.MQTT:
-                    msg.payload = rsp.mqtt_data
-                    msg.received = True
-                    n += 1
-            else:
-                # include messages in the count that have already been downloaded previously
-                n += 1
-        return n
-    def get_mqtt_message(self):
-        """
-        Function to get the first of the 'received' messages in the list, 
-        'received' meaning the payload has been downloaded from the buffer
-        of the modem.
-        Return value is a ModemMqttMessage
-        """
-        for msg in self._mqtt_messages:
-            if msg.received:
-                self._mqtt_messages.remove(msg)
-                return msg
