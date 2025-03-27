e5ed2a5692d61088207ccd368ebd6a7e01de8eab
FoxyHunter7 <80623140+FoxyHunter7@users.noreply.github.com>
2025-03-27 16:13:46 +0100

    test config_gnss

---
+++ tests/test_modem_gnss.py
@@ -0,0 +1,59 @@
+import asyncio
+import minimal_unittest as unittest
+
+from walter_modem import Modem
+from walter_modem.enums import (
+    WalterModemNetworkRegState,
+    WalterModemRspType,
+    WalterModemCMEError,
+    WalterModemGNSSSensMode,
+    WalterModemGNSSAcqMode,
+    WalterModemGNSSLocMode
+)
+from walter_modem.structs import (
+    ModemRsp,
+    WalterModemOpState
+)
+
+modem = Modem()
+modem_rsp = ModemRsp()
+
+class TestModemGNSS(unittest.AsyncTestCase):
+    async def async_setup(self):
+        await modem.begin()
+
+    async def async_teardown(self):
+        # Restore back to library defaults
+        await modem.config_gnss()
+
+    async def test_config_gnss_runs(self):
+        self.assert_true(await modem.config_gnss())
+    
+    async def test_config_gnss_correctly_sets_configuration_in_modem(self):
+        if not await modem.config_gnss(
+            sens_mode=WalterModemGNSSSensMode.MEDIUM,
+            acq_mode=WalterModemGNSSAcqMode.COLD_WARM_START,
+            loc_mode=WalterModemGNSSLocMode.ON_DEVICE_LOCATION
+        ):
+            raise RuntimeError(
+                'Failed to config GNSS',
+                WalterModemCMEError.get_value_name(modem_rsp.cme_error)
+            )
+    
+        gnss_config_str_from_modem = None
+
+        def lpgnsscfg_handler(cmd, at_rsp):
+            nonlocal gnss_config_str_from_modem
+            gnss_config_str_from_modem = at_rsp
+        
+        modem._register_application_queue_rsp_handler(b'+LPGNSSCFG: ', lpgnsscfg_handler)
+        await modem._run_cmd(at_cmd='AT+LPGNSSCFG?', at_rsp=b'OK')
+
+        for _ in range(100):
+            if gnss_config_str_from_modem is not None: break
+            await asyncio.sleep(0.1)
+        
+        self.assert_equal(b'+LPGNSSCFG: 0,2,2,,1,0,0', gnss_config_str_from_modem)
+
+test_modem_gnss = TestModemGNSS()
+test_modem_gnss.run()
