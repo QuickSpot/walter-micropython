1ac0a93e239571f3552f244f4a022a68d59d6ae9
FoxyHunter7 <80623140+FoxyHunter7@users.noreply.github.com>
2025-04-02 09:43:48 +0200

    split deepsleep test in deepsleep alone & state persistance testing

--- tests/modem_library/test_deepsleep.py
+++ tests/modem_library/test_deepsleep.py
@@ -43,6 +43,32 @@ class TestModemDeepSleep(unittest.AsyncTestCase):
         print(f'Started, reason: {machine.wake_reason()}')
         await asyncio.sleep(3)
         print('Second startup notice (3s after startup)')
+
+        if machine.wake_reason() != machine.DEEPSLEEP_RESET:
+            await modem.begin()
+            await modem.create_PDP_context()
+            await modem.get_op_state(rsp=modem_rsp)
+            if modem_rsp.op_state is not WalterModemOpState.FULL:
+                await modem.set_op_state(WalterModemOpState.FULL)
+
+            await await_connection()
+            print('Network Connection established')
+            print('Waiting 5sec before entering deepsleep')
+            await asyncio.sleep(5)
+            print('Starting 20s deepsleep')
+            modem.sleep(sleep_time_ms=20000, persist_mqtt_subs=False)
+
+    async def test_modem_begins_after_deepsleep(self):
+        self.assert_does_not_throw(await modem.begin(), Exception)
+    
+    async def test_modem_retained_connection_during_sleep(self):
+        self.assert_equal(WalterModemOpState.FULL, modem._op_state)
+
+class TestModemDeepSleepMqttPersist(unittest.AsyncTestCase):
+    async def async_setup(self):
+        print(f'Started, reason: {machine.wake_reason()}')
+        await asyncio.sleep(3)
+        print('Second startup notice (3s after startup)')
         await modem.begin()
         
         if machine.wake_reason() != machine.DEEPSLEEP_RESET:
@@ -68,5 +94,9 @@ class TestModemDeepSleep(unittest.AsyncTestCase):
     async def test_mqtt_subscriptions_persist_after_deepsleep(self):
         self.assert_equal([('short', 1), ('long-topic-test', 0)], modem._mqtt_subscriptions)
 
+
 test_modem_deep_sleep = TestModemDeepSleep()
 test_modem_deep_sleep.run()
+
+# test_modem_deep_sleep_mqtt_persist = TestModemDeepSleepMqttPersist()
+# test_modem_deep_sleep_mqtt_persist.run()
