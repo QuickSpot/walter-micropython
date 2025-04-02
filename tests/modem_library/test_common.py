291db3f6660c60f23a4b692dd61ec0817936b70f
FoxyHunter7 <80623140+FoxyHunter7@users.noreply.github.com>
2025-04-02 12:13:01 +0200

    update test_common for more thorough testing

--- tests/modem_library/test_common.py
+++ tests/modem_library/test_common.py
@@ -1,15 +1,19 @@
 import asyncio
 
 import minimal_unittest as unittest
+
 from walter_modem import Modem
-from walter_modem.enums import WalterModemOpState
+from walter_modem.enums import (
+    WalterModemOpState,
+    WalterModemCEREGReportsType,
+    WalterModemCMEErrorReportsType
+)
 from walter_modem.structs import ModemRsp
 from walter_modem.queue import QueueFull
 
 modem = Modem()
-modem_rsp = ModemRsp()
 
-class TestModemCommon(unittest.AsyncTestCase):
+class TestModemCommon(unittest.AsyncTestCase, unittest.WalterModemAsserts):
     async def test_begin_runs(self):
         await self.assert_does_not_throw(modem.begin, (
             ValueError,
@@ -32,29 +36,41 @@ class TestModemCommon(unittest.AsyncTestCase):
     
     async def test_config_cme_error_reports_runs(self):
         self.assert_true(await modem.config_cme_error_reports())
+    
+    async def test_config_cme_error_reports_sends_correct_at_cmd(self):
+        await self.assert_sends_at_command(
+            modem,
+            'AT+CMEE=1',
+            lambda: modem.config_cme_error_reports(WalterModemCMEErrorReportsType.NUMERIC)
+        )
 
     async def test_config_cereg_reports_runs(self):
         self.assert_true(await modem.config_cereg_reports())
+
+    async def test_config_cereg_reports_sends_correct_at_cmd(self):
+        await self.assert_sends_at_command(
+            modem,
+            'AT+CEREG=1',
+            lambda: modem.config_cereg_reports(WalterModemCEREGReportsType.ENABLED)
+        )
     
     async def test_get_op_state_runs(self):
         self.assert_true(await modem.get_op_state())
+
+    async def test_get_op_state_sets_op_state_in_response(self):
+        modem_rsp = ModemRsp()
+        await modem.get_op_state(rsp=modem_rsp)
+        self.assert_is_not_none(modem_rsp.op_state)
     
     async def test_set_op_state_runs(self):
         self.assert_true(await modem.set_op_state(WalterModemOpState.MINIMUM))
     
-    async def test_get_op_state_returns(self):
-        await modem.get_op_state(rsp=modem_rsp)
-        self.assert_is_not_none(modem_rsp.op_state)
-
-    async def test_set_op_state_sets(self):
-        if modem_rsp.op_state != WalterModemOpState.NO_RF:
-            await modem.set_op_state(op_state=WalterModemOpState.NO_RF, rsp=modem_rsp)
-        else:
-            await modem.set_op_state(op_state=WalterModemOpState.MINIMUM, rsp=modem_rsp)
-
-        await modem.get_op_state(rsp=modem_rsp)
-
-        self.assert_equal(WalterModemOpState.NO_RF, modem_rsp.op_state)
+    async def test_set_op_state_sends_correct_at_cmd(self):
+        await self.assert_sends_at_command(
+            modem,
+            'AT+CFUN=4',
+            lambda: modem.set_op_state(WalterModemOpState.NO_RF)
+        )
 
 test_modem_common = TestModemCommon()
 test_modem_common.run()
