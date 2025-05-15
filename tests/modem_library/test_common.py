e88929a6652cfd4f16ba7bd5ee961b9e7a2da8a4
FoxyHunter7 <80623140+FoxyHunter7@users.noreply.github.com>
2025-05-15 12:15:10 +0200

    Test reset() test_returns_true, test_resets_mirror_state, test_keeps_internal_begun_flag

--- tests/modem_library/test_common.py
+++ tests/modem_library/test_common.py
@@ -33,3 +33,34 @@ class TestBegin(
             asyncio.TimeoutError,
             asyncio.CancelledError
         ))
+
+class TestReset(
+    AsyncTestCase
+):
+    async def async_setup(self):
+        await modem.begin() # Modem begin is idempotent
+
+    async def test_returns_true(self):
+        self.assert_true(await modem.reset())
+    
+    async def test_resets_mirror_state(self):
+        # Only interested in changing mirror state for test,
+        # not the actual modem's state
+        modem._op_state = WalterModemOpState.MANUFACTURING
+        mirror_op_state_before = modem._op_state
+
+        await modem.reset()
+
+        self.assert_not_equal(mirror_op_state_before, modem._op_state)        
+    
+    async def test_keeps_internal_begun_flag(self):
+        await modem.reset()
+        self.assert_true(modem._begun)
+
+testcases = [testcase() for testcase in (
+    TestBegin,
+    TestReset,
+)]
+
+for testcase in testcases:
+    testcase.run()
