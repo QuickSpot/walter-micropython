8cc78c9cd981d690e3cae34382161695e425e25b
FoxyHunter7 <80623140+FoxyHunter7@users.noreply.github.com>
2025-05-21 16:24:33 +0200

    Refactor gnss mixin to be self contained Hooking into the core's mechanicsism and ensuring initialisation of further mixins, cleaning up if last.

--- walter_modem/mixins/gnss.py
+++ walter_modem/mixins/gnss.py
@@ -1,18 +1,73 @@
+import asyncio
+import gc
+
+from micropython import const # type: ignore
+
 from ..core import ModemCore
 from ..enums import (
     WalterModemGNSSSensMode,
     WalterModemGNSSAcqMode,
     WalterModemGNSSLocMode,
     WalterModemGNSSAssistanceType,
-    WalterModemGNSSAction
+    WalterModemGNSSAction,
+    WalterModemState,
+    WalterModemRspType
 )
 from ..structs import (
     ModemRsp,
     ModemGNSSFix,
-    ModemGnssFixWaiter
+    ModemGnssFixWaiter,
+    ModemGNSSAssistance,
+    ModemGNSSSat
+)
+from ..utils import (
+    parse_gnss_time,
+    log
 )
 
+_UNICODE_0 = const(0)
+_UNICODE_1 = const(1)
+_UNICODE_2 = const(2)
+_UNICODE_COMMA = const(44)
+_UNICODE_BRACKET_OPEN = const(40)
+_UNICODE_BRACKET_CLOSE = const(41)
+
 class ModemGNSS(ModemCore):
+    def __init__(self, *args, **kwargs):
+        if not hasattr(self, '__initialised_mixins'):
+            super().__init__(*args, **kwargs)
+
+        self.__gnss_fix_lock = asyncio.Lock()
+        self.__gnss_fix_waiters = []
+        """GNSS fix waiters"""
+
+        self.__queue_rsp_rsp_handlers = (
+            self.__queue_rsp_rsp_handlers + (
+                (b'+LPGNSSFIXREADY: ', self.__handle_lp_gnss_fix_ready),
+                (b'+LPGNSSASSISTANCE: ', self.__handle_lp_gnss_assistance),
+            )
+        )
+
+        self.__mirror_state_reset_callables = (
+            self.__mirror_state_reset_callables + (self._gnss_mirror_state_reset,)
+        )
+
+        self.__initialised_mixins.append(ModemGNSS)
+        if len(self.__initialised_mixins) == len(self.__class__.__bases__):
+            del self.__initialised_mixins
+            next_base = None
+        else:
+            next_base: callable
+            for base in self.__class__.__bases__:
+                if base not in self.__initialised_mixins:
+                    next_base = base
+                    break
+
+        gc.collect()
+        log('INFO', 'GNSS mixin loaded')
+        if next_base is not None: next_base.__init__(self, *args, **kwargs)
+    
+#region PublicMethods
 
     # Deprecated aliases, to bre removed in a later release
 
@@ -137,9 +192,156 @@ class ModemGNSS(ModemCore):
         """
         gnss_fix_waiter = ModemGnssFixWaiter()
 
-        async with self._gnss_fix_lock:
-            self._gnss_fix_waiters.append(gnss_fix_waiter)
+        async with self.__gnss_fix_lock:
+            self.__gnss_fix_waiters.append(gnss_fix_waiter)
 
         await gnss_fix_waiter.event.wait()
 
         return gnss_fix_waiter.gnss_fix
+
+#endregion
+
+#region PrivateMethods
+
+    def _gnss_mirror_state_reset(self):
+        self.__gnss_fix_lock = asyncio.Lock()
+        self.__gnss_fix_waiters = []
+
+#endregion
+
+#region QueueResponseHandlers
+
+    async def __handle_lp_gnss_fix_ready(self, tx_stream, cmd, at_rsp):
+        data = at_rsp[len(b'+LPGNSSFIXREADY: '):]
+
+        parenthesis_open = False
+        part_no = 0
+        start_pos = 0
+        part = ''
+        gnss_fix = ModemGNSSFix()
+
+        for character_pos in range(len(data)):
+            character = data[character_pos]
+            part_complete = False
+
+            if character == _UNICODE_COMMA and not parenthesis_open:
+                part = data[start_pos:character_pos]
+                part_complete = True
+            elif character == _UNICODE_BRACKET_OPEN:
+                parenthesis_open = True
+            elif character == _UNICODE_BRACKET_CLOSE:
+                parenthesis_open = False
+            elif character_pos + 1 == len(data):
+                part = data[start_pos:character_pos + 1]
+                part_complete = True
+
+            if part_complete:
+                if part_no == 0:
+                    gnss_fix.fix_id = int(part)
+                elif part_no == 1:
+                    part = part[1:-1]
+                    gnss_fix.timestamp = parse_gnss_time(part)
+                elif part_no == 2:
+                    gnss_fix.time_to_fix = int(part)
+                elif part_no == 3:
+                    part = part[1:-1]
+                    gnss_fix.estimated_confidence = float(part)
+                elif part_no == 4:
+                    part = part[1:-1]
+                    gnss_fix.latitude = float(part)
+                elif part_no == 5:
+                    part = part[1:-1]
+                    gnss_fix.longitude = float(part)
+                elif part_no == 6:
+                    part = part[1:-1]
+                    gnss_fix.height = float(part)
+                elif part_no == 7:
+                    part = part[1:-1]
+                    gnss_fix.north_speed = float(part)
+                elif part_no == 8:
+                    part = part[1:-1]
+                    gnss_fix.east_speed = float(part)
+                elif part_no == 9:
+                    part = part[1:-1]
+                    gnss_fix.down_speed = float(part)
+                elif part_no == 10:
+                     # Raw satellite signal sample is ignored
+                    pass
+                else:
+                    satellite_data = part.decode().split(',')
+
+                    # Iterate through the satellite_data list, taking every two elements as pairs
+                    for i in range(0, len(satellite_data), 2):
+                        sat_no_str = satellite_data[i]
+                        sat_sig_str = satellite_data[i + 1]
+
+                        gnss_fix.sats.append(ModemGNSSSat(int(sat_no_str[1:]), int(sat_sig_str[:-1])))
+
+                # +1 for the comma
+                part_no += 1
+                start_pos = character_pos + 1
+                part = ''
+
+        # notify every coroutine that is waiting for a fix
+        async with self.__gnss_fix_lock:
+            for gnss_fix_waiter in self.__gnss_fix_waiters:
+                gnss_fix_waiter.gnss_fix = gnss_fix
+                gnss_fix_waiter.event.set()
+
+            self.__gnss_fix_waiters = []
+        
+        return WalterModemState.OK
+
+    async def __handle_lp_gnss_assistance(self, tx_stream, cmd, at_rsp):
+        if not cmd:
+            return
+
+        if cmd.rsp.type != WalterModemRspType.GNSS_ASSISTANCE_DATA:
+            cmd.rsp.type = WalterModemRspType.GNSS_ASSISTANCE_DATA
+            cmd.rsp.gnss_assistance = ModemGNSSAssistance()
+
+        data = at_rsp[len("+LPGNSSASSISTANCE: "):]
+        part_no = 0
+        start_pos = 0
+        part = ''
+        gnss_details = None
+
+        for character_pos in range(len(data)):
+            character = data[character_pos]
+            part_complete = False
+
+            if character == _UNICODE_COMMA:
+                part = data[start_pos:character_pos]
+                part_complete = True
+            elif character_pos + 1 == len(data):
+                part = data[start_pos:character_pos + 1]
+                part_complete = True
+
+            if part_complete:
+                if part_no == 0:
+                    if part[0] == _UNICODE_0:
+                        gnss_details = cmd.rsp.gnss_assistance.almanac
+                    elif part[0] == _UNICODE_1:
+                        gnss_details = cmd.rsp.gnss_assistance.realtime_ephemeris
+                    elif part[0] == _UNICODE_2:
+                        gnss_details = cmd.rsp.gnss_assistance.predicted_ephemeris
+                elif part_no == 1:
+                    if gnss_details:
+                        gnss_details.available = int(part) == 1
+                elif part_no == 2:
+                    if gnss_details:
+                        gnss_details.last_update = int(part)
+                elif part_no == 3:
+                    if gnss_details:
+                        gnss_details.time_to_update = int(part)
+                elif part_no == 4 and gnss_details:
+                        gnss_details.time_to_expire = int(part)
+
+                # +1 for the comma
+                part_no += 1
+                start_pos = character_pos + 1
+                part = ''
+
+        return WalterModemState.OK
+
+#endregion
